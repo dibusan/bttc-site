b751b4b860a425f5554252f28a9b5a7aec045b31
Eriel Marimon <dev.eriel.mf@gmail.com>
2020-05-27 16:53:10 -0400

    Added About content

--- app/assets/javascripts/sidenav.js
+++ app/assets/javascripts/sidenav.js
@@ -3,5 +3,6 @@ $(document).on('turbolinks:load', function() {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('#content').toggleClass('padded');
+        $('#sidebarCollapseWrapper').toggleClass('padded');
     });
 });
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,36 @@
 class VisitorsController < ApplicationController
   def home
+    @sponsors = [
+      {
+        name: 'ITTF',
+        url: 'https://www.ittf.com/',
+        image: 'sponsor-ittf'
+      },{
+        name: 'USTTA',
+        url: 'https://www.teamusa.org/USA-Table-Tennis',
+        image: 'sponsor-ustta'
+      },{
+        name: 'ULTM',
+        url: 'http://ultm.org/',
+        image: 'sponsor-ultm'
+      },{
+        name: 'Butterfly',
+        url: 'https://butterflyonline.com/',
+        image: 'butterfly-logo'
+      },{
+        name: 'Newgy',
+        url: 'https://www.newgy.com/',
+        image: 'sponsor-newgy'
+      },{
+        name: 'Paddle Palace',
+        url: 'https://www.paddlepalace.com/',
+        image: 'sponsor-paddlepalace'
+      },{
+        name: 'Fort Lauderdale convention center',
+        url: 'https://www.ftlauderdalecc.com/',
+        image: 'sponsor-ftlauderdaleconvcenter'
+      }
+
+    ]
   end
 end
